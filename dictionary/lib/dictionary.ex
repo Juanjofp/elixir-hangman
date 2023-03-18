38b7c9d450aeb450dd6e71e6d0fd340bcd78f1ff
juanjo <juanjo@juanjofp.com>
2023-03-18 11:06:56 +0100

    1 - Create Dictionary module

---
+++ dictionary/lib/dictionary.ex
@@ -0,0 +1,18 @@
+defmodule Dictionary do
+
+  # Hello World
+
+  def hello() do
+    IO.puts "Hello World!!!"
+  end
+
+  def bye() do
+    IO.puts "Bye World!!!"
+  end
+
+  def run() do
+    hello()
+    bye()
+  end
+
+end
